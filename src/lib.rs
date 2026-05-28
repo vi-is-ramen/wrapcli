56b4403b2c59d506d624970f8b0dc27acee094b4
Vi Raven <vi.is.chapmann@gmail.com>
2026-05-28 03:23:38 +0300

    docs(crate): fix doc-including line

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#[doc = include_str!("../mdocs/index.md")]
+#![doc = include_str!("../mdocs/index.md")]
 use std::io::{self, BufRead, BufReader, Write};
 use std::process::{Child, Command, ExitStatus, Stdio};
 use std::thread;
